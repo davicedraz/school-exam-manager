31cbab39b602beb53dadc49c55d39ff13894d297
Davi Cedraz <davi_cedraz@atlantico.com.br>
2020-02-24 23:47:15 -0300

    initialize springboot project

---
+++ src/main/java/com/cedraz/school/exams/app/SchoolExamManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cedraz.school.exams.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolExamManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolExamManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedraz/school/exams/app/SchoolExamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedraz.school.exams.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolExamManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
