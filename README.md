593cf1c2e575454a6016b752c2b1c51e83d2f376
davicedraz <davioler@gmail.com>
2020-02-26 00:47:48 -0300

    Added swagger as documentation framework to the APIs; Create README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# School Exams Management System (SEMS)
+
+#### System for managing school tests and measure student performance.
+
+<p align="center">
+    <a alt="Java">
+        <img src="https://img.shields.io/badge/Java-v1.8-orange.svg" />
+    </a>
+    <a alt="Spring Boot">
+        <img src="https://img.shields.io/badge/Spring%20Boot-v2.2.4-brightgreen.svg" />
+    </a>     
+    <a alt="Docker">
+        <img src="https://img.shields.io/badge/Docker-v18-yellowgreen.svg" />
+    </a>
+    <a alt="Dependencies">
+        <img src="https://img.shields.io/badge/dependencies-up%20to%20date-brightgreen.svg" />
+    </a>
+    <a alt="License">
+        <img src="https://img.shields.io/badge/license-MIT-blue.svg" />
+    </a>
+</p>
--- src/main/java/com/cedraz/sas/exams/app/config/ApplicationConfig.java
+++ src/main/java/com/cedraz/sas/exams/app/config/ApplicationConfig.java
@@ -3,13 +3,51 @@ package com.cedraz.sas.exams.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableSwagger2
 public class ApplicationConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Docket swaggerUserApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Students")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cedraz.sas.exams.app.config"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("School Exam Management System - REST APIs")
+                .description("Spring Boot School Exam Management system.").termsOfServiceUrl("")
+                .contact(new Contact("Davi Cedraz", "https://www.linkedin.com/in/davicedraz/", "davioler@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .version("0.0.1")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", "Authorization", "header");
     }
 
 }
